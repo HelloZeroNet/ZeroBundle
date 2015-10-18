a10bfa72040566bb5a5dba7044e3d57197d1492a
HelloZeroNet <hello@noloop.me>
2015-10-19 00:30:02 +0200

    Call zeronet.py directly to if already downloaded to avoid problems with interactive commands

--- zeronet.cmd
+++ zeronet.cmd
@@ -2,5 +2,11 @@
 if "%1" == "" (
 	Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet start.py
 ) else (
-	Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet zeronet.py %*
+	if not exist ZeroNet (
+		Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet zeronet.py %*
+	) else (
+		cd ZeroNet
+		..\Python\python.exe zeronet.py %*
+		cd ..
+	)
 )

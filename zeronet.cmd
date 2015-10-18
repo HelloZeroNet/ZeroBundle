750bb32133c1248ae9dd2c967fb404cfa7ad14d2
HelloZeroNet <hello@noloop.me>
2015-10-18 23:46:15 +0200

    Ignore hidden files, pass startup parameters from batfile, use zeronet.py if parameters used

--- zeronet.cmd
+++ zeronet.cmd
@@ -1,1 +1,6 @@
-Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet start.py
+@echo off
+IF "%1" == "" (
+	Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet start.py
+) ELSE (
+	Python\python.exe -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet zeronet.py %*
+)

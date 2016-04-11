46cdd5ec37314b937fc143820dbce5647f773e3e
HelloZeroNet <hello@noloop.me>
2016-04-11 12:00:01 +0200

    New mac bash script

--- ZeroNet.app/Contents/MacOS/ZeroNet.sh
+++ ZeroNet.app/Contents/MacOS/ZeroNet.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+cd "$(dirname "$0")/../../../"
 if [ -d "ZeroNet" ]; then
-    cd "$(dirname "$0")/../../../ZeroNet"
-    bash ../Python/python start.py "$@"
+    cd ZeroNet
+    bash ../Python/python start.py
 else
-    cd "$(dirname "$0")/../../../"
     bash Python/python -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet start.py
 fi
+exit 0

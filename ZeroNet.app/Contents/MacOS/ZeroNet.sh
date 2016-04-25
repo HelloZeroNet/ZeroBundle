716041a09b08a274885bd4a5b22e979bbc6594c7
HelloZeroNet <hello@noloop.me>
2016-04-25 12:57:49 +0200

    Fix multi-source download

--- ZeroNet.app/Contents/MacOS/ZeroNet.sh
+++ ZeroNet.app/Contents/MacOS/ZeroNet.sh
@@ -4,6 +4,6 @@ if [ -d "ZeroNet" ]; then
     cd ZeroNet
     bash ../Python/python start.py
 else
-    bash Python/python -m zerobundle.run https://github.com/HelloZeroNet/ZeroNet start.py
+    ./Python/python -m zerobundle.run "https://github.com/HelloZeroNet/ZeroNet;https://gitlab.com/HelloZeroNet/ZeroNet;https://try.gogs.io/ZeroNet/ZeroNet" $SCRIPT "$@"
 fi
 exit 0

79d32cc966ba6d0766d6ef41f6e2ece4b344813d
shortcutme <hello@noloop.me>
2017-01-05 21:38:44 +0100

    New mac zerobundle based on apple script

--- ZeroNet.app/Contents/MacOS/ZeroNet.sh
+++ ZeroNet.app/Contents/MacOS/ZeroNet.sh
@@ -4,6 +4,7 @@ if [ -d "ZeroNet" ]; then
     cd ZeroNet
     bash ../Python/python start.py
 else
-    ./Python/python -m zerobundle.run "https://github.com/HelloZeroNet/ZeroNet;https://gitlab.com/HelloZeroNet/ZeroNet;https://try.gogs.io/ZeroNet/ZeroNet" $SCRIPT "$@"
+    osascript -e 'display notification "Downloading and starting latest version..." with title "ZeroNet"'
+    ./Python/python -m zerobundle.run "https://github.com/HelloZeroNet/ZeroNet;https://gitlab.com/HelloZeroNet/ZeroNet;https://try.gogs.io/ZeroNet/ZeroNet" "start.py" "$@"
 fi
 exit 0

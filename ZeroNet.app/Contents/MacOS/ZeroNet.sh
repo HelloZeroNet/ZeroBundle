ad4cd5eafecb01555a431d62cf5261c9a3322169
shortcutme <hello@noloop.me>
2017-01-05 21:44:09 +0100

    Revert "New mac zerobundle based on apple script"
    This reverts commit 79d32cc966ba6d0766d6ef41f6e2ece4b344813d.

--- ZeroNet.app/Contents/MacOS/ZeroNet.sh
+++ ZeroNet.app/Contents/MacOS/ZeroNet.sh
@@ -4,7 +4,6 @@ if [ -d "ZeroNet" ]; then
     cd ZeroNet
     bash ../Python/python start.py
 else
-    osascript -e 'display notification "Downloading and starting latest version..." with title "ZeroNet"'
-    ./Python/python -m zerobundle.run "https://github.com/HelloZeroNet/ZeroNet;https://gitlab.com/HelloZeroNet/ZeroNet;https://try.gogs.io/ZeroNet/ZeroNet" "start.py" "$@"
+    ./Python/python -m zerobundle.run "https://github.com/HelloZeroNet/ZeroNet;https://gitlab.com/HelloZeroNet/ZeroNet;https://try.gogs.io/ZeroNet/ZeroNet" $SCRIPT "$@"
 fi
 exit 0

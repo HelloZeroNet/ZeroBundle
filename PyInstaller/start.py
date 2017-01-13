16cb6924b74ee5a1280b287632666f00da1f3746
shortcutme <tamas@zeronet.io>
2017-01-13 17:52:50 +0100

    Copy copy msv dlls and manifest to lib dir on startup

--- PyInstaller/start.py
+++ PyInstaller/start.py
@@ -10,6 +10,16 @@ start_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(start_dir, "lib"))
 sys.path.insert(0, os.path.join(start_dir, "core"))
 
+# For best compatibility copy required dlls to lib dir
+if os.path.isfile(start_dir + "\\msvcr90.dll") and not os.path.isfile(start_dir + "\\lib\\msvcr90.dll"):
+    import shutil
+    for file_name in ("msvcm90.dll", "msvcp90.dll", "msvcr90.dll", "Microsoft.VC90.CRT.manifest"):
+        try:
+            shutil.copyfile(start_dir + "\\" + file_name, start_dir + "\\lib\\" + file_name)
+        except Exception, err:
+            print "Error copy", file_name, err
+
+
 # ZeroNet Modules
 import zeronet
 

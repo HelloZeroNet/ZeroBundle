2b53885bfc847433731e216b6777e1b387a08544
shortcutme <tamas@zeronet.io>
2017-01-18 23:25:30 +0100

    macOS support

--- PyInstaller/start.py
+++ PyInstaller/start.py
@@ -1,37 +1,140 @@
 #!/usr/bin/env python2.7
-
-
-# Included modules
 import sys
 import os
-
-if getattr(sys, 'frozen', False):
-    start_dir = os.path.dirname(sys.executable)
-elif __file__:
-    start_dir = os.path.dirname(__file__)
-
-os.chdir(start_dir)  # Change the working dir
-
-sys.path.insert(0, os.path.join(start_dir, "lib"))  # External modules
-sys.path.insert(0, os.path.join(start_dir, "core"))  # ZeroNet source code
-
-# For best compatibility copy required dlls to lib dir
-if os.path.isfile(start_dir + "\\msvcr90.dll") and not os.path.isfile(start_dir + "\\lib\\msvcr90.dll"):
-    import shutil
-    for file_name in ("msvcm90.dll", "msvcp90.dll", "msvcr90.dll", "Microsoft.VC90.CRT.manifest"):
-        try:
-            shutil.copyfile(start_dir + "\\" + file_name, start_dir + "\\lib\\" + file_name)
-        except Exception, err:
-            print "Error copy", file_name, err
+import time
 
 
-# ZeroNet Modules
+def getWorkDir():
+    if getattr(sys, 'frozen', False):
+        work_dir = os.path.dirname(os.path.abspath(sys.executable))  # Normally next to .exe
+
+        if work_dir.startswith("/Application") or work_dir.startswith("/private"):
+            # In Applcation Support
+            work_dir = os.path.expanduser("~/Library/Application Support/ZeroNet")
+            if not os.path.isdir(work_dir):
+                os.mkdir(work_dir)
+        elif ".app" in work_dir:
+            # Outside of .app
+            import re
+            work_dir = re.sub("/[^/]+\.app/.*$", "", work_dir)
+    elif __file__:
+        # Right next to .py
+        work_dir = os.path.dirname(os.path.abspath(__file__))
+
+    return work_dir
+
+
+# ZeroNet source paths
+def addSourcePaths(work_dir):
+    if sys.platform == "darwin":
+        sys.path.append(work_dir + "/core")  # Updated source
+        sys.path.append(os.path.abspath(os.path.dirname(sys.executable) + "/../Resources/core"))  # Packed-in source
+    else:
+        sys.path.insert(0, os.path.join(work_dir, "lib"))  # External modules
+        sys.path.insert(0, os.path.join(work_dir, "core"))  # ZeroNet source code
+
+
+# On Windows for best compatibility copy required dlls to lib dir
+def copyDlls(work_dir):
+    if os.path.isfile(work_dir + "\\msvcr90.dll") and not os.path.isfile(work_dir + "\\lib\\msvcr90.dll"):
+        import shutil
+        for file_name in ("msvcm90.dll", "msvcp90.dll", "msvcr90.dll", "Microsoft.VC90.CRT.manifest"):
+            try:
+                shutil.copyfile(work_dir + "\\" + file_name, work_dir + "\\lib\\" + file_name)
+            except Exception, err:
+                print "Error copy dll", file_name, err
+
+
+def setup():
+    work_dir = getWorkDir()
+    addSourcePaths(work_dir)
+    if sys.platform.startswith("win"):
+        copyDlls(work_dir)
+
+
+setup()
+
+
 import zeronet
 
 
-def main():
-    sys.argv = [sys.argv[0]]+["--open_browser", "default_browser"]+sys.argv[1:]
+gui_root = None
+
+
+def gui():
+    global gui_root
+    time.sleep(5)
+    sys.path.append("/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk")
+    sys.path.append("/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload/")
+
+    def click():
+        import webbrowser
+        webbrowser.open("http://127.0.0.1:43110")
+
+    def quit():
+        sys.exit(0)
+
+    try:
+        from Tkinter import Tk
+        gui_root = Tk()
+        gui_root.iconify()
+        gui_root.createcommand('tk::mac::ReopenApplication', click)
+        gui_root.createcommand('tk::mac::Quit', quit)
+        gui_root.mainloop()
+    except ExitCommand:
+        print "Stopping..."
+    except Exception, err:
+        print "Gui error: %s" % err
+        while 1:
+            time.sleep(1000)
+
+
+class ExitCommand(Exception):
+    pass
+
+
+def signalHandler(signal, frame):
+    raise ExitCommand()
+
+
+def main(mode="main"):
+    sys.argv = [sys.argv[0]] + ["--open_browser", "default_browser"] + sys.argv[1:]  # Open browser window
+
+    if mode == "thread":
+        # Manipulate sys.exit to send exit signal before killing the thread
+        sys_exit = sys.exit
+        def threadedExit(*args):
+            if gui_root:
+                gui_root.destroy()
+            os.kill(os.getpid(), signal.SIGUSR1)
+            sys_exit(*args)
+        sys.exit = threadedExit
+
+    # Start ZeroNet itself
     zeronet.main()
 
+    # Stop gui
+    if gui_root:
+        gui_root.destroy()
+
+    if mode == "thread":
+        # Send exit signal
+        os.kill(os.getpid(), signal.SIGUSR1)
+
+
 if __name__ == '__main__':
-    main()
+    if sys.platform == "darwin":
+        import signal
+        signal.signal(signal.SIGUSR1, signalHandler)
+        # Start dock icon click watcher on macOS
+        from threading import Thread
+        t = Thread(target=main, kwargs={"mode": "thread"})
+        t.daemon = True
+        t.start()
+
+        try:
+            gui()
+        except ExitCommand:
+            pass
+    else:
+        main()
